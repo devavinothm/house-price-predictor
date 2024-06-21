142fe25a1280976db66ed97212379a0d71021026
devavinothm <learnerdev101@gmail.com>
2024-06-21 22:49:03 +0530

    Project Completed

---
+++ README.md
@@ -0,0 +1,27 @@
+# House Price Predictor
+
+## Description
+
+This project is a simple house price predictor using a linear regression model. The model is trained on a dataset of house prices in the Bangalore area. The dataset is included in the project.
+
+## Dataset
+
+The dataset is included in the project as a CSV file. The file contains the following columns:
+
+- Place (the area in Bangalore where the house is located)
+- BHKS (the number of bedrooms, hall, and kitchen in the house)
+- Square Feet (the total area of the house in square feet)
+- Bathrooms (the number of bathrooms in the house)
+- Price (the price of the house in lakhs)
+
+## Training and Testing
+
+The model is trained on 80% of the dataset and tested on the remaining 20%. The model is evaluated using the mean squared error metric.
+
+## Connect with me
+
+- [LinkedIn](https://www.linkedin.com/in/devavinoth)
+- [Portfolio](https://deva-vinoth.web.app/)
+- [GitHub](https://github.com/devavinothm/)
+
+## Have a great day!
--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+import pandas as pd
+import pickle
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+data = pd.read_csv('cleaned_data.csv')
+model = pickle.load(open('ridge_model.pkl', 'rb'))
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations=locations)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    total_sqft = request.form.get('total_sqft')
+    bath = request.form.get('bath')
+    bhk = request.form.get('bhk')
+
+    input = pd.DataFrame([[location, total_sqft, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+
+    prediction = model.predict(input)
+
+    prediction_value = prediction[0] * 100000
+
+    formated_prediction = "{:,.2f}".format(prediction_value)
+
+    print(prediction[0])
+
+    return render_template('index.html', prediction=formated_prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
